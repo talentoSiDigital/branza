bd5585e20c5903bef2da68a6fa957fbe342f4cfa
Manuel Zorrilla <123107354+manuelzorrilladev@users.noreply.github.com>
2024-02-09 15:03:08 -0400

    Added modal in contact form and fixed form

--- src/components/Contacto.vue
+++ src/components/Contacto.vue
@@ -4,7 +4,19 @@ import splash from '../components/svgComponents/splash.vue'
 import ImageBanner from '../partials/ImageBanner.vue'
 
 import ContactForm from '../components/ContactForm.vue'
+import Loading from './Loading.vue'
+import { ref } from 'vue';
 
+const active = ref(false)
+const loadMessage = ref(false)
+
+function changeNote(note) {
+    active.value = !active.value
+}
+
+function loader() {
+    loadMessage.value = !loadMessage.value
+}
 
 </script>
 
@@ -16,40 +28,19 @@ import ContactForm from '../components/ContactForm.vue'
             <div class="text-area">
 
                 <h3>Estamos aquí para ayudarte. Completa el siguiente formulario para que podamos entrar en contacto lo
-                antes posible.</h3>
+                    antes posible.</h3>
 
             </div>
-        <div class="time-line">
+            <div class="time-line">
 
-            <!-- <div class="splash">
-                        <splash color="#00adef" class="medium" />
-                        <div class="text-splash ">
-                            <header class="white">
-                                <div class="line">
-                                    <font-awesome-icon :icon="['fas', 'envelope']" class="icon" />
-                                    <h5>branza@example.com</h5>
-                                </div>
-                                <div class="line">
-                                    <font-awesome-icon :icon="['fas', 'phone']" class="icon " />
-                                    <h5>(58) 212-000-0000</h5>
-                                        </div>
-                                        <div class="line">
-                                            <font-awesome-icon :icon="['fas', 'location-dot']" class="icon2" />
-                                            <h5>284 Royal, New Jersey</h5>
-                                        </div>
-
-
-                                    </header>
-                                </div>
-                            </div> -->
-                <div class="frame frame-glass">
+                <div class="frame">
                     <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3923.0198343323214!2d-66.82497922422601!3d10.499102364246271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a597652590ec7%3A0x7fbbed0fb5069750!2sBranza!5e0!3m2!1ses!2sve!4v1700755744999!5m2!1ses!2sve"
                         style="border:0;" allowfullscreen="" loading="lazy"
                         referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
 
-                <ContactForm class="form">
+                <ContactForm class="form" @change-note="changeNote" @loader="loader">
                     <template #title>
 
                         <h2>Déjanos un mensaje</h2>
@@ -57,15 +48,118 @@ import ContactForm from '../components/ContactForm.vue'
                     </template>
                 </ContactForm>
 
+
+
+
             </div>
         </div>
+        <Transition name="bounce">
+            <div v-if="active" class="send-alert">
+                <div class="bg-alert">
+                    <div class="flex" v-if="loadMessage">
+                        <h2>El mensaje ha sido enviado exitosamente</h2>
+                        <button @click="changeNote()">Seguir navegando</button>
+                    </div>
+                    <Loading class="load" v-else />
+
+                </div>
+
+            </div>
+
+        </Transition>
+
     </div>
 </template>
 
 <style scoped>
+.bounce-enter-active {
+    animation: bounce-in 0.5s;
+}
+
+.bounce-leave-active {
+    animation: bounce-in 0.5s reverse;
+}
+
+
+
+@keyframes bounce-in {
+    0% {
+        transform: scale(0);
+    }
+
+    50% {
+        transform: scale(1.25);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+
+.no-show {
+    display: none !important;
+}
+
+.send-alert {
+    position: fixed;
+    top: 0;
+    background: rgba(48, 68, 155, 0.4);
+    z-index: 50;
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.bg-alert {
+    background: rgba(48, 68, 155, 1);
+    color: white !important;
+    height: 30%;
+    width: 35%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border-radius: 15px;
+    min-width: fit-content;
+
+}
+
+.load {
+    width: 80%;
+}
+
+.flex {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+.bg-alert h2 {
+    text-align: center;
+    width: 80%;
+    margin-bottom: 10px;
+}
+
+.bg-alert button {
+    background: white;
+    border: none;
+    padding: 10px 15px;
+    border-radius: 10px;
+    cursor: pointer;
+    transition: 400ms;
+}
+
+.bg-alert button:hover {
+    background: #e3e3e3;
+}
+
+
 .form {
     width: 40%;
-    box-shadow: 4px 4px 0px 0px rgba(0, 0, 0, 0.15);
     padding: 15px;
 }
 
---
+++ src/components/Loading.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="load-container">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            style="margin: auto; background: none; display: block; shape-rendering: auto;" width="100px" height="100px"
+            viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+            <circle cx="50" cy="50" r="37" stroke-width="8" stroke="#fff"
+                stroke-dasharray="58.119464091411174 58.119464091411174" fill="none" stroke-linecap="round">
+                <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="2.272727272727273s"
+                    keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+            </circle>
+        </svg>
+        <h2>Procesando solicitud...</h2>
+    </div>
+</template>
+
+<style scoped>
+.load-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: white;
+}
+
+
+</style>
+
+
+
+
